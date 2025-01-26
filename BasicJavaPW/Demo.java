b229d083507258d6b72b9faa421acf555769e0a5
kamal Prajapat <kamalprajapat7117@gmail.com>
2025-01-26 11:05:36 +0530

    Add Some 7th star Note About main() method in java

---
+++ BasicJavaPW/Demo.java
@@ -0,0 +1,17 @@
+class Demo {
+    public static void main(String[] args) {
+        //All are valid Sysntex
+    //  static public void main(String ... args){
+    // public static void main(String[] args) {
+        System.out.println("hello kamal Prajapat");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+}
+
+// ////
+// java Demo kamal kumar
+// hello kamal Prajapat
+// kamal
+// kumar
+// PS D:\7thsem\Skills\NotepadJava> ///
